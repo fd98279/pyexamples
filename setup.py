0a00b24be0aff49c6d57624ab0c71eedae47c1a5
vagrant <vagrant@vagrant.vm>
2017-08-30 23:55:42 +0000

    Initial

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PyLexer',
+      version='1.0',
+      description='Rackspace Test Python Lexer',
+      author='Sampat Ponnaganti',
+      author_email='mailingsam@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['rackindexer'],
+     )
---
+++ tests/test_core.py
@@ -0,0 +1,4 @@
+import mock, pytest
+
+def test_get_filepaths():
+    assert 0 # to see what was printed
